37e75437d9d7ee05a9ef9789091c8726adbc833a
shreyansh <shreyansh@noob.local>
2021-02-11 11:14:43 +0530

    Added models for authorization

---
+++ fyle_slack_app/models/SlackTeam.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class SlackTeam(models.Model):
+    
+    class Meta:
+        db_table = 'slack_teams'
+
+    id = models.CharField(max_length=120, primary_key=True)
+    name = models.CharField(max_length=120)
+    bot_user_id = models.CharField(max_length=120)
+    bot_access_token = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.id)
---
+++ fyle_slack_app/models/__init__.py
@@ -0,0 +1,3 @@
+from .SlackTeam import SlackTeam
+from .SlackUser import SlackUser
+from .FyleEmployee import FyleEmployee
