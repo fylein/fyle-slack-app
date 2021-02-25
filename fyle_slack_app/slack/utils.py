94c3aafa4c25c867a62a39c34d686e0b4cf35eea
shreyansh <shreyansh@noob.local>
2021-02-25 15:48:08 +0530

    Separated out state encoding and decoding in functions

--- fyle_slack_app/slack/utils.py
+++ fyle_slack_app/slack/utils.py
@@ -13,15 +13,28 @@ def get_slack_user_dm_channel_id(slack_client: WebClient, user_id: str) -> str:
     return slack_user_dm_channel_id['channel']['id']
 
 
+def encode_state(state_params):
+    state = json.dumps(state_params)
+
+    encoded_state = state.encode()
+    base64_encoded_state = base64.urlsafe_b64encode(encoded_state).decode()
+
+    return base64_encoded_state
+
+
+def decode_state(state):
+    decoded_state = base64.urlsafe_b64decode(state.encode())
+    state_params = json.loads(decoded_state.decode())
+    return state_params
+
+
 def get_fyle_oauth_url(user_id: str, team_id: str) -> str:
     state_params = {
         'user_id': user_id,
         'team_id': team_id
     }
-    state = json.dumps(state_params)
 
-    encoded_state = state.encode()
-    base64_encoded_state = base64.urlsafe_b64encode(encoded_state).decode()
+    base64_encoded_state = encode_state(state_params)
 
     redirect_uri = '{}/fyle/authorization'.format(settings.SLACK_SERVICE_BASE_URL)
 
