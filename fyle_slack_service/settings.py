fdf7e0e59e95376616ee0ef172878bfcddf4b92c
shreyansh <shreyansh@noob.local>
2021-03-04 13:35:34 +0530

    Changing debug value setting logic in settings.py

--- fyle_slack_service/settings.py
+++ fyle_slack_service/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
