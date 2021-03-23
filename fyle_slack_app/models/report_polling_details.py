a881f63d864917ba07fc1a97375cb60372abd9ec
shreyansh <shreyansh@noob.local>
2021-03-23 15:53:51 +0530

    Squashed migrtations into 1 and added required indexes

--- fyle_slack_app/models/report_polling_details.py
+++ fyle_slack_app/models/report_polling_details.py
@@ -8,8 +8,8 @@ class ReportPollingDetail(models.Model):
     class Meta:
         db_table = 'report_polling_details'
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    last_successful_poll_at = models.DateTimeField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    last_successful_poll_at = models.DateTimeField(db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- fyle_slack_app/models/users.py
+++ fyle_slack_app/models/users.py
@@ -7,12 +7,12 @@ class User(models.Model):
     class Meta:
         db_table = 'users'
 
-    slack_user_id = models.CharField(max_length=120, primary_key=True)
+    slack_user_id = models.CharField(max_length=120, unique=True)
     slack_team = models.ForeignKey(Team, on_delete=models.CASCADE)
     slack_dm_channel_id = models.CharField(max_length=120, unique=True)
     email = models.EmailField()
-    fyle_refresh_token = models.TextField()
-    fyle_employee_id = models.CharField(max_length=120, null=True)
+    fyle_refresh_token = models.TextField(db_index=True)
+    fyle_employee_id = models.CharField(max_length=120)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
