b5103c5e6eb35b231362ce3c59fb1e168cf28f43
Shreyansh <shreyansh.sahare@fyle.in>
2021-09-03 10:51:11 +0530

    Removed sentry user context

--- fyle_slack_service/__init__.py
+++ fyle_slack_service/__init__.py
@@ -2,12 +2,3 @@ from django.http import JsonResponse
 
 def ready(request):
     return JsonResponse({'message': 'slack app service is ready'}, status=200)
-
-
-def traces_sampler(sampling_context):
-    # avoiding ready APIs in performance tracing
-    if sampling_context.get('wsgi_environ') is not None:
-        if sampling_context['wsgi_environ']['PATH_INFO'] in ['/ready']:
-            return 0
-
-    return 0.2
