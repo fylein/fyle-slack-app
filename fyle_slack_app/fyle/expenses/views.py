62e90f02e0cc51f293295a9184655f88819fc1b2
Shreyansh <shreyansh.sahare@fyle.in>
2021-10-22 09:40:06 +0530

    Added expense form dialog changes

---
+++ fyle_slack_app/fyle/expenses/views.py
@@ -0,0 +1,202 @@
+from typing import Dict, List
+
+from fyle.platform.platform import Platform
+
+from fyle_slack_app.fyle.utils import get_fyle_sdk_connection
+from fyle_slack_app.libs.utils import decode_state, encode_state
+from fyle_slack_app.models.users import User
+from fyle_slack_app.fyle import utils as fyle_utils
+
+
+class FyleExpense:
+
+    connection: Platform = None
+
+    def __init__(self, user: User) -> None:
+        self.connection = get_fyle_sdk_connection(user.fyle_refresh_token)
+
+
+    def get_expense_fields(self, query_params: Dict) -> Dict:
+        return self.connection.v1.fyler.expense_fields.list(query_params=query_params)
+
+
+    def get_default_expense_fields(self) -> Dict:
+        default_expense_fields_query_params = {
+            'offset': 0,
+            'limit': '20',
+            'order': 'created_at.desc',
+            'column_name': 'in.(purpose, txn_dt, vendor_id, cost_center_id, project_id)',
+            'is_enabled': 'eq.{}'.format(True),
+            'is_custom': 'eq.{}'.format(False),
+            'is_mandatory': 'eq.{}'.format(True)
+        }
+
+        return self.get_expense_fields(default_expense_fields_query_params)
+
+
+    def get_custom_fields_by_category_id(self, category_id: str) -> Dict:
+        custom_fields_query_params = {
+            'offset': 0,
+            'limit': '20',
+            'order': 'created_at.desc',
+            'column_name': 'not_in.(purpose, txn_dt, vendor_id, cost_center_id)',
+            'is_enabled': 'eq.{}'.format(True),
+            'category_ids': 'cs.[{}]'.format(int(category_id))
+        }
+
+        return self.get_expense_fields(custom_fields_query_params)
+
+
+    def get_categories(self, query_params: Dict) -> Dict:
+        return self.connection.v1.fyler.categories.list(query_params=query_params)
+
+
+    def get_projects(self, query_params: Dict) -> Dict:
+        return self.connection.v1.fyler.projects.list(query_params=query_params)
+
+
+    def get_cost_centers(self, query_params: Dict) -> Dict:
+        return self.connection.v1.fyler.cost_centers.list(query_params=query_params)
+
+
+    def get_expenses(self, query_params: Dict) -> Dict:
+        return self.connection.v1.fyler.expenses.list(query_params=query_params)
+
+
+    def get_reports(self, query_params: Dict) -> Dict:
+        return self.connection.v1.fyler.reports.list(query_params=query_params)
+
+
+    @staticmethod
+    def get_currencies():
+        return ['ADP','AED','AFA','ALL','AMD','ANG','AOA','ARS','ATS','AUD','AWG','AZM','BAM','BBD','BDT','BEF','BGL','BGN','BHD','BIF','BMD','BND','BOB','BOV','BRL','BSD','BTN','BWP','BYB','BZD','CAD','CDF','CHF','CLF','CLP','CNY','COP','CRC','CUP','CVE','CYP','CZK','DEM','DJF','DKK','DOP','DZD','ECS','ECV','EEK','EGP','ERN','ESP','ETB','EUR','FIM','FJD','FKP','FRF','GBP','GEL','GHC','GIP','GMD','GNF','GRD','GTQ','GWP','GYD','HKD','HNL','HRK','HTG','HUF','IDE','IDR','IEP','ILS','INR','IQD','IRR','ISK','ITL','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LUF','LVL','LYD','MAD','MDL','MGF','MKD','MMK','MNT','MOP','MRO','MTL','MUR','MVR','MWK','MXN','MXV','MYR','MZM','NAD','NGN','NIO','NLG','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PTE','PYG','QAR','ROL','RUB','RUR','RWF','RYR','SAR','SBD','SCR','SDP','SEK','SGD','SHP','SIT','SKK','SLL','SOS','SRG','STD','SVC','SYP','SZL','THB','TJR','TMM','TND','TOP','TPE','TRL','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYU','UZS','VEB','VND','VUV','WST','XAF','XCD','XDR','XEU','XOF','XPF','YER','YUN','ZAR','ZMK','ZRN','ZWD']
+
+
+    @staticmethod
+    def get_expense_fields_type_mandatory_mapping(expense_fields: List[Dict]) -> Dict:
+        mandatory_mapping = {
+            'purpose': False,
+            'txn_dt': False,
+            'vendor_id': False,
+            'project_id': False,
+            'cost_center_id': False
+        }
+
+        for field in expense_fields['data']:
+            if field['column_name'] in mandatory_mapping:
+                mandatory_mapping[field['column_name']] = field['is_mandatory']
+
+        return mandatory_mapping
+
+
+    @staticmethod
+    def get_expense_form_details(user: User) -> Dict:
+
+        fyle_expense = FyleExpense(user)
+
+        fyle_profile = fyle_utils.get_fyle_profile(user.fyle_refresh_token)
+
+        home_currency = fyle_profile['org']['currency']
+
+        default_expense_fields = fyle_expense.get_default_expense_fields()
+
+        field_type_mandatory_mapping = fyle_expense.get_expense_fields_type_mandatory_mapping(default_expense_fields)
+
+        is_project_available = False
+        is_cost_center_available = False
+
+        projects_query_params = {
+            'offset': 0,
+            'limit': '1',
+            'order': 'created_at.desc',
+            'is_enabled': 'eq.{}'.format(True)
+        }
+
+        projects = fyle_expense.get_projects(projects_query_params)
+
+        is_project_available = True if projects['count'] > 0 else False
+
+        cost_centers_query_params = {
+            'offset': 0,
+            'limit': '1',
+            'order': 'created_at.desc',
+            'is_enabled': 'eq.{}'.format(True)
+        }
+
+        cost_centers = fyle_expense.get_cost_centers(cost_centers_query_params)
+
+        is_cost_center_available = True if cost_centers['count'] > 0 else False
+
+        # Create a expense fields render property and set them optional in the form
+        fields_render_property = {
+            'project': {
+                'is_project_available': is_project_available,
+                'is_mandatory': field_type_mandatory_mapping['project_id']
+            },
+            'cost_center': {
+                'is_cost_center_available': is_cost_center_available,
+                'is_mandatory': field_type_mandatory_mapping['cost_center_id']
+            },
+            'purpose': field_type_mandatory_mapping['purpose'],
+            'transaction_date': field_type_mandatory_mapping['txn_dt'],
+            'vendor': field_type_mandatory_mapping['vendor_id']
+        }
+
+        additional_currency_details = {
+            'home_currency': home_currency
+        }
+
+        add_to_report = 'existing_report'
+
+        # Caching details in slack private metadata so that they can be reused again in the form without computing them again
+        private_metadata = {
+            'fields_render_property': fields_render_property,
+            'additional_currency_details': additional_currency_details,
+            'add_to_report': add_to_report
+        }
+
+        expense_form_details = {
+            'fields_render_property': fields_render_property,
+            'private_metadata': encode_state(private_metadata),
+            'additional_currency_details': additional_currency_details,
+            'add_to_report': add_to_report
+        }
+
+        return expense_form_details
+
+
+    @staticmethod
+    def get_current_expense_form_details(slack_payload: Dict) -> Dict:
+
+        private_metadata = slack_payload['view']['private_metadata']
+
+        decoded_private_metadata = decode_state(private_metadata)
+
+        fields_render_property = decoded_private_metadata['fields_render_property']
+
+        additional_currency_details = decoded_private_metadata.get('additional_currency_details')
+
+        add_to_report = decoded_private_metadata.get('add_to_report')
+
+        current_ui_blocks = slack_payload['view']['blocks']
+
+        project = decoded_private_metadata.get('project')
+
+        custom_field_blocks = []
+
+        for block in current_ui_blocks:
+            if 'custom_field' in block['block_id'] or 'additional_field' in block['block_id']:
+                custom_field_blocks.append(block)
+
+        if len(custom_field_blocks) == 0:
+            custom_field_blocks = None
+
+        current_form_details = {
+            'fields_render_property': fields_render_property,
+            'selected_project': project,
+            'additional_currency_details': additional_currency_details,
+            'add_to_report': add_to_report,
+            'private_metadata': private_metadata,
+            'custom_fields': custom_field_blocks
+        }
+        return current_form_details
