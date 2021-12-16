249f8c13144ed1decff605c857297d9d2f3ea426
shreyanshss7 <72918700+shreyanshss7@users.noreply.github.com>
2021-12-16 11:01:30 +0530

    Catching integrity error (#54)
    * Catching integrity error
    
    * integrity error message change

--- fyle_slack_service/exception_middleware.py
+++ fyle_slack_service/exception_middleware.py
@@ -1,6 +1,7 @@
 import traceback
 
 from django.http import JsonResponse
+from django.db import IntegrityError
 from django.utils.deprecation import MiddlewareMixin
 
 from fyle_slack_app.libs.assertions import InvalidUsage
@@ -14,6 +15,11 @@ class CustomExceptionMiddleware(MiddlewareMixin):
         if isinstance(exception, InvalidUsage):
             resp = exception.to_dict()
             status = exception.status_code
+        if isinstance(exception, IntegrityError):
+            resp = {
+                'message': 'Seems like an error occured from our side. Please contact support@fylehq.com'
+            }
+            status = 400
         else:
             code = 500
             resp = {
