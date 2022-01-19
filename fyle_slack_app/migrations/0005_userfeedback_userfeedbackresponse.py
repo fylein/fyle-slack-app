07428ba0523273f1428f5b1e8c0683adf118c276
Shreyansh <shreyansh.sahare@fyle.in>
2022-01-19 16:08:55 +0530

    Renamed migration

--- fyle_slack_app/migrations/0005_userfeedback_userfeedbackresponse.py
+++ fyle_slack_app/migrations/0005_userfeedback_userfeedbackresponse.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('fyle_slack_app', '0003_usersubscriptiondetail'),
+        ('fyle_slack_app', '0004_user_fyle_org_id'),
     ]
 
     operations = [
