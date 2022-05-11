707821bb6462c82a300c2afd69f75fbc92802148
shreyanshs7 <shreyanshss7@gmail.com>
2022-05-11 12:51:11 +0530

    Reverted qcluster workers to 4

--- fyle_slack_service/settings.py
+++ fyle_slack_service/settings.py
@@ -152,7 +152,7 @@ Q_CLUSTER = {
     'name': 'fyle_slack_service',
     'compress': True,
     'save_limit': 0,
-    'workers': 1,
+    'workers': 4,
     'queue_limit': 50,
     'orm': 'default',
     'ack_failures': True,
