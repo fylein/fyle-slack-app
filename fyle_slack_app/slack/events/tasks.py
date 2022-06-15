b3ea4cd87d2034a7ae986072dc6a138c6d3c1bff
Jatin Sharma <jatin.sharma@fyle.in>
2022-06-15 17:18:09 +0530

    Add trackers for expense creation from slack (#87)
    * Add trackers for expense creation from slack
    
    * Minor changes

--- fyle_slack_app/slack/events/tasks.py
+++ fyle_slack_app/slack/events/tasks.py
@@ -162,6 +162,9 @@ def handle_file_shared(file_id: str, user_id: str, team_id: str):
         except assertions.InvalidUsage:
             error_message = 'Seems like something went wrong while creating an expense, please try again or contact support@fylehq.com'
             slack_client.chat_postMessage(channel=user.slack_dm_channel_id, text=error_message, thread_ts=message_ts, reply_broadcast=True)
+
+        FyleExpense(user).track_expense_creation(user, 'Expense created from uploading Receipt in Slack', expense['id'])
+        
         return None
 
 
