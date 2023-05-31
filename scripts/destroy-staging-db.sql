34fe4ea0950cc5f815aacb5e72fe43576dbdcd87
ashwin1111 <winash1998@gmail.com>
2023-05-31 17:25:09 +0530

    Update bot access token if the org is already connected

---
+++ scripts/destroy-staging-db.sql
@@ -0,0 +1,10 @@
+-- rollback;
+-- begin;
+
+-- delete from notification_preferences;
+-- delete from report_polling_details;
+-- delete from teams;
+-- delete from user_feedback_responses;
+-- delete from user_feedbacks;
+-- delete from user_subscription_details;
+-- delete from users;
