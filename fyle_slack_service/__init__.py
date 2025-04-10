138f3af9e2b7fc298396c16a04bd7af13ac700aa
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-04-10 22:06:45 +0530

    chore: add db call in ready api

--- fyle_slack_service/__init__.py
+++ fyle_slack_service/__init__.py
@@ -1,4 +1,8 @@
 from django.http import JsonResponse
+from fyle_slack_app.models.users import User
+
 
 def ready(request):
+    User.objects.first()
+
     return JsonResponse({'message': 'slack app service is ready'}, status=200)
