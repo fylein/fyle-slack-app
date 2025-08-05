e7c47c4c5c13ac563213245307427eab46908075
ruuushhh <rushikeshtayade2001@gmail.com>
2025-08-05 17:51:28 +0530

    fix: slack app loading

--- fyle_slack_service/__init__.py
+++ fyle_slack_service/__init__.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
-from fyle_slack_app.models.users import User
 
 
 def ready(request):
+    from fyle_slack_app.models.users import User
     User.objects.first()
 
     return JsonResponse({'message': 'slack app service is ready'}, status=200)
